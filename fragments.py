0767c705416d935b914d1dea36b74abd8d80d01e
Clyde-fare <clyde.fare@gmail.com>
2014-07-25 16:41:13 +0100

    Added docstrings

--- fragments.py
+++ fragments.py
@@ -2,20 +2,22 @@ import itertools
 
 unwind = lambda l: (e for sl in l for e in sl)
 
-
 class GraphFrags(object):
+    """Class to split a graphitic fragment into subfragments"""
     def __init__(self, master):
         self.master = master
-        self.list_rings = self.get_all_rings()
-        self.ring_lookup = self.get_ring_lookup()
-        self.shell_lookup = []
         self.base_frag = tuple([])
+        self.shell_lookup = []
+        self.ring_lookup = self.get_ring_lookup()
+        self.list_rings = self.get_all_rings()
 
     def set_base_frag(self, frag):
+        """Base fragment from which all fragments are generated from and to which all fragments are connected to"""
         self.base_frag = tuple(frag)
         self.shell_lookup = self._get_shell_lookup()
 
     def length_n_frags(self, initial, n=6):
+        """Returns all chains of length n that start at carbon initial"""
         frags = []
         current_frag = initial
         if len(current_frag) >= n:
@@ -29,6 +31,7 @@ class GraphFrags(object):
         return frags
 
     def get_all_rings(self, initial_ring=None, list_rings=None):
+        """Recursively generates all rings connected to initial_ring"""
         if not list_rings:
             list_rings = []
 
@@ -42,6 +45,7 @@ class GraphFrags(object):
         return list_rings
 
     def get_ring_lookup(self, initial_ring=None):
+        """Returns a lookup dictionary specifying the ring neighbors of each ring in the fragment"""
         rings = self.get_all_rings(initial_ring=initial_ring)
 
         lookup = {}
@@ -51,6 +55,7 @@ class GraphFrags(object):
         return lookup
 
     def _get_ring_neighbors(self, initial_ring):
+        """"Returns a list of ring neighbors connected to initial_ring"""
         initial_indexes = []
         for i in initial_ring:
             if not any([n in initial_indexes for n in self.master.graph.neighbors[i]]):
@@ -65,6 +70,7 @@ class GraphFrags(object):
         return rings
 
     def neighbor_combinations(self, neighbors):
+        """"Generator of a all possible combinations of the list of rings neighbors"""
         no_neighbors = len(neighbors)
 
         for i in range(1, no_neighbors+1):
@@ -72,6 +78,7 @@ class GraphFrags(object):
                 yield n_combination
 
     def get_rings(self, initial):
+        """Returns the list of rings atom initial is part of """
         frags = self.length_n_frags(tuple([initial]))
         rings = []
         for f in frags:
@@ -82,6 +89,8 @@ class GraphFrags(object):
         return rings
 
     def redundant_frag(self, frag):
+        """Determines whether fragment frag contains holes - i.e. does not include a ring explicitly that is included implicitly because
+        all atoms in that ring are included in other rings that are included in the fragment"""
         frag_atoms = set(unwind(frag))
 
         for ring in self.list_rings:
@@ -91,6 +100,7 @@ class GraphFrags(object):
         return False
 
     def get_frag_neighbors(self, frag):
+        """Returns tuple of all neighboring rings for a given fragment"""
         frag_neighbors = []
         for n in unwind(self.ring_lookup[r] for r in frag):
             if n not in frag and n not in frag_neighbors:
@@ -98,6 +108,7 @@ class GraphFrags(object):
         return tuple(frag_neighbors)
 
     def _get_shell_lookup(self):
+        """Returns a list of list of rings, element i of this list corresponds to the list of rings for shell i"""
         shell = 1
         allowed_rings = []
         shell_rings = self._get_allowed_rings(shell)
@@ -126,12 +137,14 @@ class GraphFrags(object):
         return frag_neighbors
 
     def get_allowed_rings(self, shell):
+        """Returns the rings belonging to shell shell"""
         try:
             return self.shell_lookup[shell-1]
         except IndexError:
             return tuple([])
 
     def _gen_fragments(self, base_frag, allowed_rings=None):
+        """Generator of fragments build from base_frag and the list of rings specifed in allowed_rings"""
         frag_neighbors = []
         for n in (unwind(self.ring_lookup[r] for r in base_frag)):
             if n not in frag_neighbors:
@@ -148,7 +161,7 @@ class GraphFrags(object):
                 yield frag
 
     def gen_fragments(self, origin_frag=None, shell=0):
-
+        """Recursively generates fragments starting from origin_frag that are part of shell shell"""
         if not origin_frag:
             origin_frag = self.base_frag
         else:
@@ -170,6 +183,7 @@ class GraphFrags(object):
 
 
 def get_uniques(frags):
+    """Get unique fragments"""
     atoms = lambda e: set(unwind(e))
     list_atoms = []
     for f in frags:
@@ -184,18 +198,19 @@ def get_uniques(frags):
 
 
 def _test_1a():
+    """Tests the number of fragments found matches a known case"""
     from ase.io import read
     import ASE_utils
     from math import pi
 
-    base_33_test = read('test1.xyz')
-    base_33_test.rotate('x', pi/2)
-    base_33_test.rotate('z', pi/2)
+    test_mol = read('test1.xyz')
+    test_mol.rotate('x', pi/2)
+    test_mol.rotate('z', pi/2)
 
-    m_33_test = ASE_utils.to_molmod(base_33_test)
-    m_33_test.set_default_graph()
+    m_test_mol = ASE_utils.to_molmod(test_mol)
+    m_test_mol.set_default_graph()
 
-    g = GraphFrags(m_33_test)
+    g = GraphFrags(m_test_mol)
     r = g.get_rings(10)[-1]
     g.set_base_frag([r])
     g_it = g.gen_fragments()
@@ -204,8 +219,7 @@ def _test_1a():
 
 
 def _test_1b():
-    """Check no redundant fragments produced"""
-
+    """Tests no redundant fragments are produced"""
     from ase.io import read
     import ASE_utils
     from math import pi
