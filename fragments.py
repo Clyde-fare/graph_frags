4ceb19974eb61ac645d6a216118799d3f7906933
Clyde-fare <clyde.fare@gmail.com>
2014-07-25 11:22:40 +0100

    Switched to generator rather than list for fragments
    Add a check for redundancies  associated with holes - i.e. instances where a ring is not present in a fragment but all neighbouring rings are which means in fact the ring is present because all it's atoms are present in the surrounding neighbors.
    
     Thus we check for the presence of structure with such holes and do not include them in the generator

--- fragments.py
+++ fragments.py
@@ -1,32 +1,36 @@
 import itertools
 
-    
 unwind = lambda l: (e for sl in l for e in sl)
+
 
 class Graph_frags(object):
     def __init__(self, master):
         self.master = master
+        self.list_rings = self.get_all_rings()
         self.ring_lookup = self.get_ring_lookup()
         self.list_fragments = []
+        self.shell_lookup = []
+        self.base_frag = tuple([])
 
     def clear(self):
         self.list_fragments = []
-        
-    def length_6_frags(self, initial):
+
+    def set_base_frag(self, frag):
+        self.base_frag = tuple(frag)
+        self.shell_lookup = self._get_shell_lookup()
+
+    def length_n_frags(self, initial, n=6):
         frags = []
         current_frag = initial
-        if len(current_frag) >= 6:
+        if len(current_frag) >= n:
             return [current_frag]
-        
+
         neighbor_inds = self.master.graph.neighbors[current_frag[-1]]
         for neighbor_ind in neighbor_inds:
             if neighbor_ind not in current_frag:
-                #new_frag=copy.copy(current_frag)
-                #new_frag.append(neighbor_ind)
                 new_frag = current_frag + (neighbor_ind, )
-                frags += self.length_6_frags(new_frag)
+                frags += self.length_n_frags(new_frag, n)
         return frags
-    
 
     def get_all_rings(self, initial_ring=None, list_rings=None):
         if not list_rings:
@@ -46,7 +50,7 @@ class Graph_frags(object):
 
         lookup = {}
         for ring in rings:
-            lookup["-".join(map(str,ring))] = self._get_ring_neighbors(ring)
+            lookup["-".join(map(str, ring))] = self._get_ring_neighbors(ring)
 
         return lookup
 
@@ -63,9 +67,9 @@ class Graph_frags(object):
         rings = []
         for r in (r for lr in init_rings for r in lr):
             if r not in rings and r != initial_ring:
-                rings.append(r)        
+                rings.append(r)
         return rings
-    
+
     def get_ring_neighbors(self, initial_ring):
         return self.ring_lookup["-".join(map(str, initial_ring))]
 
@@ -78,33 +82,32 @@ class Graph_frags(object):
 
         return itertools.chain(*list_n_combinations)
 
-
     def get_rings(self, initial):
-        frags=self.length_6_frags(tuple([initial]))
+        frags = self.length_n_frags(tuple([initial]))
         rings = []
         for f in frags:
             if initial in self.master.graph.neighbors[f[-1]]:
-                ring=sorted(f)
+                ring = sorted(f)
                 if ring not in rings:
                     rings.append(ring)
         rings.sort()
         return rings
-    
-    def get_ring_fragments(self, initial_frags=None, allowed_rings=None):
 
+    def get_ring_fragments(self, initial_frags=None, allowed_rings=None):
+        """The list version"""
         if not initial_frags:
             initial_frags = [[self.get_rings(0)[0]]]
 
         for frag in initial_frags:
             self.list_fragments.append(frag)
-            
+
         if allowed_rings is None:
             neighbors = unwind([self.get_ring_neighbors(r) for initial_frag in initial_frags for r in initial_frag])
             current_rings = [sorted(r) for initial_frag in initial_frags for r in initial_frag]
             allowed_rings = [sorted(r) for r in neighbors if sorted(r) not in current_rings]
 
         new_frags = []
-                        
+
         for frag in initial_frags:
             new_neighbors = []
             for ring in frag:
@@ -112,7 +115,8 @@ class Graph_frags(object):
                     if n in allowed_rings and not n in new_neighbors:
                         new_neighbors.append(n)
 
-            new_frags += [tuple(frag) + neighbor_combination for neighbor_combination in self.neighbor_combinations(new_neighbors)]
+            new_frags += [tuple(frag) + neighbor_combination for neighbor_combination
+                          in self.neighbor_combinations(new_neighbors)]
 
         if not new_frags:
             return
@@ -124,24 +128,112 @@ class Graph_frags(object):
                     allowed_rings.append(n)
 
         self.get_ring_fragments(initial_frags=new_frags, allowed_rings=allowed_rings)
-        
-        
+
+    def redundant_frag(self, frag):
+        frag_atoms = set(unwind(frag))
+
+        for ring in self.list_rings:
+            if ring not in frag:
+                if set(ring).issubset(frag_atoms):
+                    return True
+        return False
+
+    def get_frag_neighbors(self, frag):
+        frag_neighbors = []
+        for n in unwind(self.get_ring_neighbors(r) for r in frag):
+            if n not in frag and n not in frag_neighbors:
+                frag_neighbors.append(n)
+        return tuple(frag_neighbors)
+
+    def _get_shell_lookup(self):
+        shell = 1
+        allowed_rings = []
+        shell_rings = self._get_allowed_rings(shell)
+        while shell_rings:
+            shell += 1
+            allowed_rings.append(shell_rings)
+            shell_rings = self._get_allowed_rings(shell)
+
+        return allowed_rings
+
+    def _get_allowed_rings(self, shell, base_frag=None):
+        """Gets the nth shell of neighbours from self.base_frag"""
+
+        if not base_frag and not self.base_frag:
+            raise(RuntimeError('Require a base fragment from which to compute which rings belong to the shell'))
+        elif base_frag:
+            shell_base_frag = base_frag
+        else:
+            shell_base_frag = self.base_frag
+
+        frag_neighbors = []
+        for i in range(shell):
+            frag_neighbors = self.get_frag_neighbors(shell_base_frag)
+            shell_base_frag = shell_base_frag + frag_neighbors
+
+        return frag_neighbors
+
+    def get_allowed_rings(self, shell):
+        try:
+            return self.shell_lookup[shell-1]
+        except IndexError:
+            return tuple([])
+
+    def _gen_fragments(self, base_frag, allowed_rings=None):
+        frag_neighbors = []
+        for n in (unwind([self.get_ring_neighbors(r) for r in base_frag])):
+            if n not in frag_neighbors:
+                frag_neighbors.append(n)
+
+        if allowed_rings is None:
+            allowed_rings = frag_neighbors
+
+        allowed_neighbors = [n for n in frag_neighbors if n in allowed_rings]
+
+        for neighbor_combination in self.neighbor_combinations(allowed_neighbors):
+            frag = tuple(base_frag) + neighbor_combination
+            if not self.redundant_frag(frag):
+                yield frag
+
+    def gen_fragments(self, origin_frag=None, shell=0):
+
+        if not origin_frag:
+            origin_frag = self.base_frag
+        else:
+            origin_frag = tuple(origin_frag)
+
+        if shell == 0:
+            yield origin_frag
+            shell += 1
+
+        allowed_rings = self.get_allowed_rings(shell)
+        generation_1, generation_2 = itertools.tee(self._gen_fragments(origin_frag, allowed_rings))
+        for frag in generation_1:
+            yield frag
+
+        shell += 1
+        for frag in generation_2:
+            for next_frag in self.gen_fragments(frag, shell=shell):
+                yield next_frag
+
+
 def _test_1():
-    import ASE_utils
-    import math
     from ase.io import read
+    import ASE_utils
+    from math import pi
 
     base_33_test = read('3_3_test2_flat_graphene.xyz')
-    base_33_test.rotate('x', math.pi/2)
-    base_33_test.rotate('z', math.pi/2)
+    base_33_test.rotate('x', pi/2)
+    base_33_test.rotate('z', pi/2)
 
     m_33_test = ASE_utils.to_molmod(base_33_test)
     m_33_test.set_default_graph()
 
     g = Graph_frags(m_33_test)
     r = g.get_rings(10)[-1]
-    g.get_ring_fragments(initial_frags=[[r]])
-    
-    assert len(g.list_fragments) == 112
+    g.set_base_frag([r])
+    g_it = g.gen_fragments()
+
+    assert len(list(g_it)) == 112
     
 _test_1()
