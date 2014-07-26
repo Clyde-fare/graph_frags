74e9724eb96ad4e3086aa8b02d2ce8cf1da7a036
Clyde-fare <clyde.fare@gmail.com>
2014-07-26 14:30:20 +0100

    Moved get_unique to utils

--- tests.py
+++ tests.py
@@ -1,21 +1,7 @@
 __author__ = 'clyde'
 
-from utils import unwind
+from utils import get_unique
 from fragments import GraphFrags
-
-def get_unique(frags):
-    """Get unique fragments"""
-    atoms = lambda e: set(unwind(e))
-    list_atoms = []
-    for f in frags:
-        list_atoms.append(atoms(f))
-
-    unique_atoms = []
-    for a in list_atoms:
-        if a not in unique_atoms:
-            unique_atoms.append(a)
-
-    return unique_atoms
 
 
 def _test_0():
