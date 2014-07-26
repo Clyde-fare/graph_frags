c9d399aa70165d0fb9bae10c3c7d879ba15400b5
Clyde-fare <clyde.fare@gmail.com>
2014-07-26 17:41:31 +0100

    switched order of get_pair_stability so that the fragment being analysed is the first argument - useful for parallelising

--- huckel.py
+++ huckel.py
@@ -45,6 +45,6 @@ def huckel_e(frag, master):
     return eig_values.dot(electrons_per_level)
 
 
-def get_pair_stability(pair, frag, master):
+def get_pair_stability(frag, pair, master):
     cut_frag = [set(r)-pair for r in frag]
     return huckel_e(frag, master) - huckel_e(cut_frag, master)
