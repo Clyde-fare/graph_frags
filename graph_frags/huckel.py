5725dfdeff97afb685491d9faeef2b184eb7d0d3
Clyde-fare <clyde.fare@gmail.com>
2015-04-08 13:41:03 +0100

    added explicit dependence on carbons and assorted other updates

--- graph_frags/huckel.py
+++ graph_frags/huckel.py
@@ -7,30 +7,72 @@ from utils import unwind
 #params that define the huckel energy
 alpha, beta = -11.2, -0.7
 
-def huckel_e(frag, master):
-    """Compute the Huckel energy of a fragment"""
+def get_neighbour_matrix(frag, master):
+    """Get the neighbour matrix associated with sp2 carbons in a particular PAH"""
 
     if frag:
         ns = {}
-        set_atoms = set(unwind(frag))
-        no_atoms = len(set_atoms)
-
+        #sp2 atoms in fragment (assuming all atoms are carbons)
+        set_atoms = [a for a in set(unwind(frag)) if len(master.graph.neighbors[a]) <= 3]
         for ring in frag:
-                for i in ring:
+            for i in ring:
+                #include sp2 atoms in the fragment
+                if i in set_atoms:
                     ns[i] = [n for n in master.graph.neighbors[i] if n in set_atoms]
 
     else:
-        ns = master.graph.neighbors
-        no_atoms = len(ns)
+        #dict of atom:[atom_neighbours] for sp2 atoms in total molecule (assumes all atoms are carbons)
+        ns = {}
+
+        set_atoms = [k for k in master.graph.neighbors if len(master.graph.neighbors[k]) <= 3]
+
+        for k in set_atoms:
+            ns[k] = [n for n in master.graph.neighbors[k] if n in set_atoms]
 
     ks = sorted(ns.keys())
     h = np.zeros([len(ns), len(ns)])
 
     for i, k in enumerate(ks):
-        h[i][i] = alpha
+        h[i][i] = 0
         for k2 in ns[k]:
             j = ks.index(k2)
-            h[i][j] = beta
+            h[i][j] = 1
+
+    return h
+
+#not trivially invertible i.e. vec_to_frag not easy
+def frag_to_vec(frag, master, size=None):
+    """Create vector representation of a PAH fragment
+    vector is formed of the sorted eigenvalues of the neighbour (bond)  matrix of the fragment"""
+    h = get_neighbour_matrix(frag, master)
+
+    no_atoms = len(h)
+    if size:
+        extras = size - no_atoms
+        h = np.pad(h, ((0,extras),(0,extras)), mode='constant', constant_values=0)
+
+    eig_values, eig_vectors = linalg.eigh(h)
+
+    eig_values.sort()
+    return eig_values
+
+def frag_to_vec2(frag, master):
+    fragment = set(list(unwind(frag)))
+    vec = np.zeros(len(master))
+
+    for c in fragment:
+        vec[c] = 1
+
+    return vec
+
+def huckel_e(frag, master):
+    """Compute the Huckel energy of a fragment"""
+
+    h = get_neighbour_matrix(frag, master) * beta
+    no_atoms = len(h)
+
+    for i in range(no_atoms):
+        h[i][i] = alpha
 
     eig_values, eig_vectors = linalg.eigh(h)
 
